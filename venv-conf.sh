9b06a8df5b9794388004e70252bb9ec98ea42111
MaksJanu <xia2017@wp.pl>
2025-12-05 21:14:16 +0100

    chores(venv setup script for WSL machine): Set up script for desktop.

---
+++ venv-conf.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+echo "📚 Setting up Auth Service..."
+cd backend/auth-service
+python3.12 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+deactivate
+cd ../..
+
+
+echo "📚 Setting up API Gateway..."
+cd backend/gateway
+python3.12 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+deactivate
+cd ../..
+
+
+echo "📚 Setting up User Service..."
+cd backend/user-service
+python3.12 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+deactivate
+cd ../..
