599fb007cdfa8f6587b777dac8997db206354829
Maksymilian Januszewski <m.januszewski.517@studms.ug.edu.pl>
2026-02-18 20:25:39 +0100

    build(docker-compose and running script): Added necessary code to the main docker cluster infrastructure and corrected the script.

--- docker-restart.sh
+++ docker-restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker compose down
+docker compose down -v
 sleep 2
-docker compose up -d --build auth-redis auth-service api-gateway shared-postgres-db user-service recipe-service shared-mongo-db frontend workout-service forum-service
+docker compose up -d --build auth-redis auth-service api-gateway shared-postgres-db user-service recipe-service shared-mongo-db frontend workout-service forum-service 
