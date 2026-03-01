ae0f5a6d2531d6ea2aecf0c151952a9f03c2231d
Maksymilian Januszewski <m.januszewski.517@studms.ug.edu.pl>
2026-03-01 18:39:55 +0100

    feature: Refactor of the service to use data from recipe-service to calculate based on data from it.

---
+++ backend/analytics-service/src/models/__init__.py
@@ -0,0 +1,10 @@
+from src.models.meal_entry import MealType, MacroNutrients, MealIngredient, MealEntry
+from src.models.daily_log import DailyLog
+
+__all__ = [
+    "MealType",
+    "MacroNutrients",
+    "MealIngredient",
+    "MealEntry",
+    "DailyLog",
+]
