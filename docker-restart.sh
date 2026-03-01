d2eec5cbd428c9b9ea3926233e49839df1114108
Maksymilian Januszewski <m.januszewski.517@studms.ug.edu.pl>
2026-03-01 18:45:17 +0100

    build: Updated env clause in the newly created analytics-service and additional depends-on clause to prevent too early analytics-service startup before recipe-service. Modified startup script.

--- docker-restart.sh
+++ docker-restart.sh
@@ -2,4 +2,4 @@
 
 docker compose down -v
 sleep 2
-docker compose up -d --build auth-redis auth-service api-gateway shared-postgres-db user-service recipe-service shared-mongo-db frontend workout-service forum-service 
+docker compose up -d --build auth-redis auth-service api-gateway shared-postgres-db user-service recipe-service shared-mongo-db frontend workout-service forum-service analytics-service
